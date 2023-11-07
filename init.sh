8853db22ee10a7b1bd7f3c9a4f8229b10aab851d
rivkyrizel <rivkyrizel@gmail.com>
2023-11-07 13:26:10 +0200

    add version for building

--- init.sh
+++ init.sh
@@ -8,10 +8,11 @@ read -p "Enter the version:" version
 # Define variables for image and container names
 image_name="django"
 container_name="my-django-app"
+volume_url="sqlite_db"
 
 
 # Build the Docker image using the Dockerfile
 docker build -t $image_name:$version .
 
 # Run the Docker container in the background and map port 8000 to a port on the host
-docker run -d -p 8000:8000 --name $container_name $image_name:$version
+docker run -d -p 8000:8000 --name $container_name -v $volume_url:/app/db.sqlite3 $image_name:$version
